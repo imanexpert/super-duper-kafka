e69d81bdd718c24e36be9d260f3365e737abe78b
Aaron Davis <aarondavis@Aarons-MacBook-Pro.local>
2023-11-07 21:38:10 -0700

    removed diffend

--- scripts/install_postgres.sh
+++ scripts/install_postgres.sh
@@ -1,5 +1,2 @@
 sudo apt-get install postgres
 sudo apt-get update
-
-#!/bin/bash
-export PATH=/home/ubuntu/.rvm/gems/ruby-2.5.0/bin:/home/ubuntu/.rvm/gems/ruby-2.5.0@global/bin:/home/ubuntu/.rvm/rubies/ruby-2.5.0/bin:/home/ubuntu/bin:/home/ubuntu/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/ubuntu/.rvm/bin:/home/ubuntu/.rvm/bin
