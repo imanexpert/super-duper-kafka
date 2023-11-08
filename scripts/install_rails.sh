5d12d48d31db3f994fcbb85e724b0dc55742d329
Aaron Davis <aarondavis@Aarons-MacBook-Pro.local>
2023-11-07 18:46:32 -0700

    appsec updated

---
+++ scripts/install_rails.sh
@@ -0,0 +1,20 @@
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+apt update
+apt install yarn
+
+apt install rails
+bundle install
+yarn install
+# Create db
+bundle exec rake db:create
+bundle exec rails db:migrate
+# Create test db
+bundle exec rake db:test:prepare
+# Seed dev db
+bundle exec rails db:seed
+# Create and bootstrap needed app topics
+bundle exec karafka topics migrate
+# Bootstrap the web UI
+bundle exec karafka-web migrate
