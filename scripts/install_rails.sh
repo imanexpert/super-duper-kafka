f1b5513e5a1b8ac23d4476139b0a2310ba3e17d2
Aaron Davis <aarondavis@Aarons-MacBook-Pro.local>
2023-11-07 21:20:27 -0700

    removed diffend

--- scripts/install_rails.sh
+++ scripts/install_rails.sh
@@ -1,12 +1,14 @@
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
+
+#!/bin/bash
+export PATH=/home/ubuntu/.rvm/gems/ruby-2.5.0/bin:/home/ubuntu/.rvm/gems/ruby-2.5.0@global/bin:/home/ubuntu/.rvm/rubies/ruby-2.5.0/bin:/home/ubuntu/bin:/home/ubuntu/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/ubuntu/.rvm/bin:/home/ubuntu/.rvm/bin
+sudo kill -9 $(cat /home/ubuntu/tmp/pids/server.pid)
 cd /home/ubuntu
 
 gem install bundler — user-install
-gem uninstall diffend
 bundle install
 sudo apt-get update
-gem install bundler
 sudo apt-get -y install yarn
 sudo apt-get -y update
