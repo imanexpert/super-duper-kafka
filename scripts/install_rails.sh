5fed578494b520295f7071abb6c9e33a8e57a74f
Aaron Davis <aarondavis@Aarons-MacBook-Pro.local>
2023-11-07 23:37:54 -0700

    updating ports

--- scripts/install_rails.sh
+++ scripts/install_rails.sh
@@ -1,5 +1,5 @@
 cd ~
-sudo apt -y install git curl libssl-dev libreadline-dev zlib1g-dev autoconf bison build-essential libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev
+sudo apt -y install git curl libssl-dev libreadline-dev zlib1g-dev autoconf bison build-essential libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev libpq-dev ruby-dev
 curl -fsSL https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer | bash
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
@@ -8,5 +8,6 @@ source ~/.bashrc
 echo "gem: --no-document" > ~/.gemrc
 sudo gem install bundler
 sudo gem install rails
+sudo bundle install
 
 sudo docker-compose up -d
