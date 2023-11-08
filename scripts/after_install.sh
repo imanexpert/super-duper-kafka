9c149cd8a038cda9c7b34ffa075d14a23d7915fb
Aaron Davis <aarondavis@Aarons-MacBook-Pro.local>
2023-11-07 20:41:07 -0700

    boom

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,13 +1,4 @@
-bundle install
-yarn install
-# Create db
-bundle exec rake db:create
-bundle exec rails db:migrate
-# Create test db
-bundle exec rake db:test:prepare
-# Seed dev db
-bundle exec rails db:seed
-# # Create and bootstrap needed app topics
-# bundle exec karafka topics migrate
-# # Bootstrap the web UI
-# bundle exec karafka-web migrate
+# Create and bootstrap needed app topics
+bundle exec karafka topics migrate
+# Bootstrap the web UI
+bundle exec karafka-web migrate
