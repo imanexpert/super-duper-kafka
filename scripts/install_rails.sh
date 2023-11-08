d0764bd6ac5e2f87ae5fc3790efddb8e9e313dab
Aaron Davis <aarondavis@Aarons-MacBook-Pro.local>
2023-11-07 21:35:54 -0700

    removed diffend

--- scripts/install_rails.sh
+++ scripts/install_rails.sh
@@ -1,9 +1,9 @@
-sudo apt install git curl libssl-dev libreadline-dev zlib1g-dev autoconf bison build-essential libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev
+sudo apt -y install git curl libssl-dev libreadline-dev zlib1g-dev autoconf bison build-essential libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev
 curl -fsSL https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer | bash
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 source ~/.bashrc
-rbenv install 3.2.2
+rbenv -y install 3.2.2
 rbenv global 2.7.6
 echo "gem: --no-document" > ~/.gemrc
 gem install bundler
