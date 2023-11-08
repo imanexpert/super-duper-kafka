1441de34339a48a33b1be42199ea2a846134665d
Aaron Davis <aarondavis@Aarons-MacBook-Pro.local>
2023-11-07 20:11:08 -0700

    boom

---
+++ scripts/after_install.sh
@@ -0,0 +1,11 @@
+# Create db
+bundle exec rake db:create
+bundle exec rails db:migrate
+# Create test db
+bundle exec rake db:test:prepare
+# Seed dev db
+bundle exec rails db:seed
+# Create and bootstrap needed app topics
+bundle exec karafka topics migrate
+# Bootstrap the web UI
+bundle exec karafka-web migrate
--- scripts/install_rails.sh
+++ scripts/install_rails.sh
@@ -12,17 +12,6 @@ sudo gem install puma
 sudo gem install karafka-web
 sudo apt-get -y install yarn
 sudo apt-get -y update
-docker-compose up -d
-# sudo bundle install
-# sudo yarn install
-# # Create db
-# bundle exec rake db:create
-# bundle exec rails db:migrate
-# # Create test db
-# bundle exec rake db:test:prepare
-# # Seed dev db
-# bundle exec rails db:seed
-# # Create and bootstrap needed app topics
-# bundle exec karafka topics migrate
-# # Bootstrap the web UI
-# bundle exec karafka-web migrate
+
+sudo bundle install
+sudo yarn install
