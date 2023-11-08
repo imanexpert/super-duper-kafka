dc4367bba4ff6e53384589664c0d6966237ee6f9
Aaron Davis <aarondavis@Aarons-MacBook-Pro.local>
2023-11-07 19:19:49 -0700

    checking

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -10,4 +10,3 @@ sudo systemctl status docker
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-docker-compose up -d
