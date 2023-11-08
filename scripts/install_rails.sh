1347760ef1ea15f589eaa2f3b92a78ef19926787
Aaron Davis <aarondavis@Aarons-MacBook-Pro.local>
2023-11-07 21:15:04 -0700

    removed diffend

--- scripts/install_rails.sh
+++ scripts/install_rails.sh
@@ -1,7 +1,11 @@
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
+cd /home/ubuntu
 
+gem install bundler — user-install
+gem uninstall diffend
+bundle install
 sudo apt-get update
 gem install bundler
 sudo apt-get -y install yarn
