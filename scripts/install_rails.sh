9b1e476352d3b38a987cac6b02159a2528b496a6
Aaron Davis <aarondavis@Aarons-MacBook-Pro.local>
2023-11-07 19:41:49 -0700

    bundle karafka

--- scripts/install_rails.sh
+++ scripts/install_rails.sh
@@ -3,11 +3,11 @@ echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/source
 
 
 sudo apt-get update
-sudo apt-get install ruby ruby-dev
+sudo apt-get -y install ruby ruby-dev
 sudo gem install bundler
 sudo gem install rails 
-sudo apt-get install yarn
-sudo apt-get update
+sudo apt-get -y install yarn
+sudo apt-get -y update
 sudo bundle add karafka --version ">= 2.2.4"
 # sudo bundle add karafka-web
 # docker-compose up -d
