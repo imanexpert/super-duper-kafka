9fe79a137d26301c7852faef7e1b5cb74e76ee1f
Aaron Davis <aarondavis@Aarons-MacBook-Pro.local>
2023-11-07 23:49:01 -0700

    updating ports

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-cd ~
 sudo apt-get -y update
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -7,6 +6,7 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 apt-cache policy docker-ce
 sudo apt install docker-ce
 sudo systemctl status docker
+sudo apt -y install docker-compose
 
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
